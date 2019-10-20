b83f9d2e06bf2ce6030c1f7d40ff37749ff187d8
Hutch Crowley <tothecoda@gmail.com>
2019-10-20 16:36:45 -0500

    update stable build 3 - add props needed

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ function App() {
 					</div>{' '}
 					<div className="timer">00:00</div>{' '}
 					<div className="away">
-						<h2 className="away__name"> Tigers </h2> <div className="away__score"> {awayScore} </div>{' '}
+						<h2 className="away__name"> Tigers </h2>
+						<div className="away__score"> {awayScore} </div>{' '}
 					</div>{' '}
 				</div>{' '}
 				<BottomRow />
