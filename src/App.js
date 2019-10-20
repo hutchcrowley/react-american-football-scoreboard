26af6dc4e4d6236221f3ecb6d82fca5a6ce21e33
Hutch Crowley <tothecoda@gmail.com>
2019-10-19 23:56:32 -0500

    Update functional build with hooks and setter functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from 'react'
 import './index.css'
 import BottomRow from './BottomRow'
-import { useState } from 'react'
+import React, { useState } from 'react'
+// import React, { useEffect } from 'react'
 
 function App() {
 	//TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
-	const [homeScore, setHomeScore] = useState(0)
-
-	const [awayScore, setAwayScore] = useState(0)
+	const homeScore = useState(0)
 	console.log(homeScore)
+
+	const awayScore = useState(0)
+	console.log(awayScore)
 
 	return (
 		<div className="container">
@@ -21,7 +22,7 @@ function App() {
 						{/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 						<div className="home__score"> {homeScore} </div>{' '}
 					</div>{' '}
-					<div className="timer"> 00: 03 </div>{' '}
+					<div className="timer">00.00</div>{' '}
 					<div className="away">
 						<h2 className="away__name"> Tigers </h2> <div className="away__score"> {awayScore} </div>{' '}
 					</div>{' '}
