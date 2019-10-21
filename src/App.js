f464cd124f1b39b30d06e5ee4f7d38108e74a972
Hutch Crowley <tothecoda@gmail.com>
2019-10-21 00:27:02 -0500

    Update stable build 5, add event handlers

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import BottomRow from './BottomRow'
 import React, { useState } from 'react'
 // import React, { useEffect } from 'react'
 
-function App() {
+function App ()
+{
 	//TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
 	const [homeScore, setHomeScore] = useState(0)
@@ -34,12 +35,12 @@ function App() {
 				<div className="homeButtons">
 					{' '}
 					{/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}{' '}
-					<button className="homeButtons__touchdown"> Home Touchdown </button>{' '}
-					<button className="homeButtons__fieldGoal"> Home Field Goal </button>{' '}
+					<button onClick={() => setHomeScore(homeScore + 7)} className="homeButtons__touchdown"> Home Touchdown </button>{' '}
+					<button onClick={() => setHomeScore(homeScore + 3)} className="homeButtons__fieldGoal"> Home Field Goal </button>{' '}
 				</div>{' '}
 				<div className="awayButtons">
-					<button className="awayButtons__touchdown"> Away Touchdown </button>{' '}
-					<button className="awayButtons__fieldGoal"> Away Field Goal </button>{' '}
+					<button onClick={() => setAwayScore(awayScore + 7)} className="awayButtons__touchdown"> Away Touchdown </button>{' '}
+					<button onClick={() => setAwayScore(awayScore + 3)} className="awayButtons__fieldGoal"> Away Field Goal </button>{' '}
 				</div>{' '}
 			</section>{' '}
 		</div>
